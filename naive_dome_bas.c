bb756941e6cc9ae95a6083cd0956dfe143b51443
ISNTL [Backup Account] <77953022+iatomato@users.noreply.github.com>
2021-07-19 20:03:22 +0800

    Create naive_dome_bas.c

---
+++ naive_dome_bas.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define INIT_0  ((int) 0x00000000)
+
+static time_t i;
+static char rd_s[11];
+static unsigned int F_siz = INIT_0;
+static unsigned int E_siz = INIT_0;
+
+int naive_pmatch(int *arr, int size, int m)
+{
+        i = time(NULL);
+        if ((size % 2) | 0) {
+                F_siz = (size + 1) / 2;
+                E_siz = (size - 1) / 2;
+        }
+
+        /* OK -> F_siz
+         * NO_OK -> E_siz
+         */
+        sprintf(rd_s, "%ld", i);
+        int _r = *(rd_s + 9) - '0';
+
+        /* It is ok */
+        if (!((_r % 2) | 0)) {
+                printf("F Part\n");
+                for (; F_siz >= 0; F_siz--) {
+                        if (!(m ^ *(arr + F_siz) | 0))
+                                return 1;
+                }
+                return 0;
+        } else {
+                printf("E Part\n");
+                for (; E_siz < size; E_siz++) {
+                        if (!(m ^ *(arr + E_siz)))
+                                return 1;
+                }
+                return 0;
+        }
+}
